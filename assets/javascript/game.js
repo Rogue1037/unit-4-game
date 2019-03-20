294e05ec5528e63270e9f0c095577d7c0d64d016
l3 <rogue1037@gmail.com>
2019-03-19 21:18:46 -0700

    added to css, images, start game function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,8 +29,54 @@ var options = {
         specialAttack: 50,
 
     },
-    
+
 }
+
+var userCharacter;
+var enemyCharacter;
+var player;
+var computer;
+var enemyCount;
+var playerHP;
+var enemyHP;
+var playerAttack;
+var playerName;
+var opponetName;
+var arr;
+var element;
+
+
+function startGame(){
+    userCharacter = false;
+    enemyCharacter = false;
+    player = "";
+    computer = "";
+    enemyCount = 3;
+    element = document.getElementsByClassName("choice");
+    arr = jQuery.makeArray(element);
+    $(".charChoice").css({"background" : "crimson"});
+    $("ackbar").text(options.ackbar.health);
+    $("phatTrooper").text(options.phatTrooper.health);
+    $("chewie").text(options.chewie.health);
+    $("jediATAT").text(options.jediATAT.health);
+    $("#enemyArena").empty();
+
+
+
+
+}
+
+initializeGame();
+
+$("choice").on("click", function(){
+
+    if (userCharacter === false && enemyCharacter === false){
+        player = options[$(this).data("gg-type")];
+        playerName = $(this).data("gg-type");
+        $(this).appendTo("#charArena");
+        $("#chars > .charchoice").appendTo(#enemies)
+    }
+})
 
 })
 
