e3515e530048b3ac3849c7c938566a0743d25132
l3 <rogue1037@gmail.com>
2019-03-20 00:04:47 -0700

    ran into issues. changed things. DId not help

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,84 +1,76 @@
-$document.ready(function(){
+$(document).ready(function(){
+    var characters = {
+        "ackbar": {
+            name: "Ackbar",
+            health: 90,
+            attack: 6,
+            imageUrl : "assets/images/ackbar.jpg",
+            counter : 10,
+            special: 24
 
-var options = {
-    phatTrooper: {
-        health: 100,
-        attack: 4,
-        counter: 12,
-        specialAttack: 24,
-    },
+        },
+        "phatTrooper": {
+            name: "PhatTrooper",
+            health: 100,
+            attack: 4,
+            imageUrl: "assets/images/phattrooper.jpg",
+            counter : 12,
+            special: 36
+        },
+        "chewie": {
+            name: "Chewie",
+            health: 110,
+            attack: 10,
+            imageUrl: "assets/images/cartoon_Chewie.jpg",
+            counter: 14,
+            special: 46,
+        },
+        "jediATAT": {
+            name: "JediATAT",
+            health: 120,
+            attack: 14,
+            imageUrl: "assets/images/jedi_ATAT.gif",
+            counter: 16,
+            special: 50
 
-    ackbar : {
-        health: 90,
-        attack: 6,
-        counter: 10,
-        specialAttack: 32,
-    },
+        }
+    };
+    console.log(characters);
 
-    chewie : {
-        health: 110,
-        attack: 10,
-        counter: 12,
-        specialAttack: 42,
-    },
+    var characterOne = function(character, renderArea) {
+        var charDiv = $("<div class='character' data-name='" + character.name + "'>");
+        var charName = $("<div class='character-name'>").text(character.name);
+        var charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
+        var charHealth = $("div class='character-health'>").text(character.health);
+        charDiv.append(charName).append(charImage).append(charHealth);
+        $(renderArea).append(charDiv);
+        
 
-    jediATAT : {
-        health: 120,
-        attack: 8,
-        counter: 14,
-        specialAttack: 50,
-
-    },
-
-}
-
-var userCharacter;
-var enemyCharacter;
-var player;
-var computer;
-var enemyCount;
-var playerHP;
-var enemyHP;
-var playerAttack;
-var playerName;
-var opponetName;
-var arr;
-var element;
-
-
-function startGame(){
-    userCharacter = false;
-    enemyCharacter = false;
-    player = "";
-    computer = "";
-    enemyCount = 3;
-    element = document.getElementsByClassName("choice");
-    arr = jQuery.makeArray(element);
-    $(".charChoice").css({"background" : "crimson"});
-    $("ackbar").text(options.ackbar.health);
-    $("phatTrooper").text(options.phatTrooper.health);
-    $("chewie").text(options.chewie.health);
-    $("jediATAT").text(options.jediATAT.health);
-    $("#enemyArena").empty();
-
-
-
-
-}
-
-initializeGame();
-
-$("choice").on("click", function(){
-
-    if (userCharacter === false && enemyCharacter === false){
-        player = options[$(this).data("gg-type")];
-        playerName = $(this).data("gg-type");
-        $(this).appendTo("#charArena");
-        $("#chars > .charchoice").appendTo(#enemies)
     }
-})
+    console.log(characterOne);
 
-})
+    var renderCharacters = function(charObj, areaRender) {
+        if (areaRender === ".characterSelection"){
+            $(areaRender).empty();
+            for (var key in charObj) {
+                if(charObj.hasOwnProperty(key)) {
+                    renderOne(charObj[key], areaRender);
+                }
+
+            }
+        }
+    }
+renderCharacters(characters, ".characterSelection");
+   
+});
+
+
+
+
+
+
+
+
 
 
 
