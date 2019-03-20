18370f03cef1a42b57c22dab0678ed44247f04fc
l3 <rogue1037@gmail.com>
2019-03-19 19:48:37 -0700

    added to css and started character development

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+$document.ready(function(){
+
+var options = {
+    phatTrooper: {
+        health: 100,
+        attack: 4,
+        counter: 12,
+        specialAttack: 24,
+    },
+
+    ackbar : {
+        health: 90,
+        attack: 6,
+        counter: 10,
+        specialAttack: 32,
+    },
+
+    chewie : {
+        health: 110,
+        attack: 10,
+        counter: 12,
+        specialAttack: 42,
+    },
+
+    jediATAT : {
+        health: 120,
+        attack: 8,
+        counter: 14,
+        specialAttack: 50,
+
+    },
+    
+}
+
+})
+
+
+
+
+
+
+
+
